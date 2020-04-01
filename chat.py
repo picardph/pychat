7386f7084c8a235935febdb7129c3d00f9700c49
Philip Picard <emailmeforfreeipad@gmail.com>
2020-04-01 14:46:55 -0400

    Chat now works. Working on UI.

--- chat.py
+++ chat.py
@@ -1,10 +1,151 @@
-def host():
-    pass
+import threading
+import socket
+import enum
+import math
 
 
-def connect():
-    pass
+class ChatState(enum.Enum):
+    idle = 0
+    host_is_waiting = 1
+    hosting = 2
+    connected = 3
 
 
-def disconnect():
-    pass
+MESSAGE_START_TEXT = 0
+MESSAGE_TEXT = 1
+
+
+class Chat:
+    def __init__(self, username):
+        self.__done = True
+        self.__socket = None
+        self.__client = None
+        self.__ip = ''
+        self.__port = 0
+        self.__state = ChatState.idle
+        self.__name = username
+
+    def host(self, host, port):
+        # The server must be running on a different thread to keep things responsive.
+        threading.Thread(target=self.__run_host, args=(host, port)).start()
+
+    def connect(self, host, port):
+        threading.Thread(target=self.__run_client, args=(host, port)).start()
+
+    def is_done(self):
+        return self.__done
+
+    def stop(self):
+        if not self.__done:
+            self.__done = True
+            self.__client.close()
+            self.__socket.close()
+        else:
+            raise RuntimeError('Unable to stop when chat was never started.')
+
+    def get_address(self):
+        return self.__ip
+
+    def get_port(self):
+        return self.__port
+
+    def get_state(self):
+        return self.__state
+
+    def get_username(self):
+        return self.__name
+
+    def get_connected_username(self):
+        return ''
+
+    def send_message(self, message):
+        if self.__state == ChatState.hosting or self.__state == ChatState.connected:
+            num = math.ceil(len(message) / 1023)
+            if num > 255:
+                raise RuntimeError('Message is to long to send.')
+
+            # Send the start message to tell the client there is a text message coming
+            # and how many packets to prepare for.
+            start_msg = bytearray()
+            start_msg.append(MESSAGE_START_TEXT)
+            start_msg.append(num)
+            self.__client.send(start_msg)
+
+            for i in range(num):
+                # If we are on the last message then there may not be
+                # 1023 bytes remaining so we will just grab whatever is left.
+                place = i * 1023
+                msg = bytearray()
+                msg.append(MESSAGE_TEXT)
+                if i == num - 1:
+                    msg += message[place:].encode('utf-8')
+                else:
+                    msg += message[place:place+1023].encode('utf-8')
+                self.__client.send(msg)
+        else:
+            raise RuntimeError('Must be connected to send a message.')
+
+    def __run_client(self, host, port):
+        self.__done = False
+        self.__state = ChatState.connected
+        self.__ip = host
+        self.__port = port
+
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__socket.connect((host, port))
+        # We use the same socket when we are not the host.
+        self.__client = self.__socket
+
+        self.__listen()
+
+    def __run_host(self, host, port):
+        self.__done = False
+        self.__state = ChatState.host_is_waiting
+        self.__ip = host
+        self.__port = port
+
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__socket.bind((host, port))
+        self.__socket.listen(1)
+
+        self.__listen()
+
+    def __listen(self):
+        while not self.__done:
+            if self.__state == ChatState.host_is_waiting:
+                # Unlike with the FTP project, the chat program can only have one
+                # client at a time so it does not make sense to start a thread for
+                # each client that connects. Instead I have a state machine and the program
+                # switches states depending on if there is a client connected.
+                self.__client, address = self.__socket.accept()
+
+                self.__state = ChatState.hosting
+            elif self.__state == ChatState.hosting or self.__state == ChatState.connected:
+                data = bytearray(self.__client.recv(1024))
+                print(data)
+
+                if len(data) == 0:
+                    continue
+
+                # The first byte will always be used to indicate
+                # what kind of message was sent.
+                if int(data[0]) == MESSAGE_START_TEXT:
+                    # We support text messages of infinite length so this message will tell
+                    # us how many more text messages are coming that will hold the final reassembled
+                    # message. Well in theory it is not infinite. There are 1023 characters in each
+                    # message (first byte is for message type) and only a 8-bit number is used to indicate
+                    # the number of messages. So 1023 * 255 = 260865. 260,865 characters can be sent. I'm not
+                    # going to allow more than one byte to hold the message number because then we
+                    # need to deal with the endianess of the computers and I don't want to deal with that.
+                    num = int(data[1])
+                    message = ''
+                    for i in range(num):
+                        data = bytearray(self.__client.recv(1024))
+                        if int(data[0]) != MESSAGE_TEXT:
+                            raise RuntimeError('Expected a text-formatted message.')
+                        message += data[1:].decode('utf-8')
+                        print(message)
+                elif int(data[0]) == MESSAGE_TEXT:
+                    raise RuntimeError('Expected a text message after a start-text message.')
--- gui.py
+++ gui.py
@@ -1,10 +1,8 @@
 import tkinter
 import tkinter.messagebox
 import tkinter.simpledialog
-import json
-import appdirs
-import os
 import re
+import chat
 
 # This is a hack to get emojis to work with tkinter because
 # tkinter has a bug (I confirmed this with a developer from
@@ -62,6 +60,7 @@ class ConnectDialog(tkinter.Toplevel):
         self.__master = master
         # By default we will assume the operation was canceled.
         self.__canceled = True
+        self.__name = ''
         self.__address = ''
         self.__port = ''
 
@@ -74,13 +73,17 @@ class ConnectDialog(tkinter.Toplevel):
 
         # Create the body of the dialog where the user will enter
         # the address and port number to connect to.
-        tkinter.Label(body, text='Address:').grid(row=0, column=0, padx=5, pady=5)
-        self.__address_entry = tkinter.Entry(body)
-        self.__address_entry.grid(row=0, column=1, padx=5, pady=5)
+        tkinter.Label(body, text='My name:').grid(row=0, column=0, padx=5, pady=5)
+        self.__name_entry = tkinter.Entry(body)
+        self.__name_entry.grid(row=0, column=1, padx=5, pady=5)
 
-        tkinter.Label(body, text='Port:').grid(row=1, column=0, padx=5, pady=5)
+        tkinter.Label(body, text='Address:').grid(row=1, column=0, padx=5, pady=5)
+        self.__address_entry = tkinter.Entry(body)
+        self.__address_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        tkinter.Label(body, text='Port:').grid(row=2, column=0, padx=5, pady=5)
         self.__port_entry = tkinter.Entry(body)
-        self.__port_entry.grid(row=1, column=1, padx=5, pady=5)
+        self.__port_entry.grid(row=2, column=1, padx=5, pady=5)
 
         # Create the buttons on the bottom.
         buttons = tkinter.Frame(self)
@@ -104,6 +107,7 @@ class ConnectDialog(tkinter.Toplevel):
 
     def __ok(self, event=None):
         # Copy over the values that the user entered.
+        self.__name = self.__name_entry.get()
         self.__address = self.__address_entry.get()
         self.__port = self.__port_entry.get()
 
@@ -118,71 +122,14 @@ class ConnectDialog(tkinter.Toplevel):
     def was_canceled(self):
         return self.__canceled
 
+    def get_username(self):
+        return self.__name
+
     def get_address(self):
         return self.__address
 
     def get_port(self):
         return self.__port
-
-
-class SettingsDialog(tkinter.Toplevel):
-    def __init__(self, master, **kw):
-        super().__init__(master, **kw)
-
-        self.__master = master
-
-        # Style the window like a dialog box.
-        self.transient(master)
-        self.resizable(False, False)
-
-        body = tkinter.Frame(self)
-        body.pack(padx=5, pady=5)
-
-        # Create the body of the dialog where the user will enter
-        # the new settings.
-        tkinter.Label(body, text='Username:').grid(row=0, column=0, padx=5, pady=5)
-        self.__name_entry = tkinter.Entry(body)
-        self.__name_entry.grid(row=0, column=1, padx=5, pady=5)
-
-        tkinter.Label(body, text='Port:').grid(row=1, column=0, padx=5, pady=5)
-        self.__port_spin = tkinter.Spinbox(body, from_=0, to=65535)
-        self.__port_spin.grid(row=1, column=1, padx=5, pady=5)
-
-        # Create the buttons on the bottom.
-        buttons = tkinter.Frame(self)
-        ok_btn = tkinter.Button(buttons, text='OK', width=10, default=tkinter.ACTIVE, command=self.__ok)
-        ok_btn.pack(side=tkinter.RIGHT, padx=5, pady=5)
-        cancel_btn = tkinter.Button(buttons, text='Cancel', width=10, command=self.__cancel)
-        cancel_btn.pack(side=tkinter.RIGHT, padx=5, pady=5)
-        buttons.pack()
-
-        tkinter.Label(body, text='Port:')
-
-        self.bind('<Return>', self.__ok)
-        self.bind('<Escape>', self.__cancel)
-
-        self.geometry("+%d+%d" % (master.winfo_rootx() + 50, master.winfo_rooty() + 50))
-
-        # Fill out the dialog with the current settings.
-        self.__name_entry.delete(0, tkinter.END)
-        self.__port_spin.delete(0, tkinter.END)
-        self.__name_entry.insert(0, master.get_username())
-        self.__port_spin.insert(0, master.get_default_port())
-
-        # Give ourselves focus since we are a popup dialog. Unlike the connection dialog
-        # we will not give focus to the text box right away since by default a user does
-        # not change the settings.
-        self.grab_set()
-        self.wait_window(self)
-
-    def __ok(self, event=None):
-        self.master.save_settings(self.__name_entry.get(), self.__port_spin.get())
-        self.__master.focus_set()
-        self.destroy()
-
-    def __cancel(self):
-        self.__master.focus_set()
-        self.destroy()
 
 
 class ChatTk(tkinter.Tk):
@@ -191,33 +138,7 @@ class ChatTk(tkinter.Tk):
         self.title('pychat')
         self.geometry('400x300')
 
-        # Load the current settings from the JSON file if there are already
-        # settings. If there is no settings file then we will create one with
-        # default values.
-        self.__path = appdirs.user_config_dir('pychat')
-        self.__user = '[name not set]'
-        self.__port = '5138'
-        # We may need to make the path if it does not already exist.
-        if not os.path.isdir(self.__path):
-            os.makedirs(self.__path)
-        self.__path += '/settings.json'
-        if os.path.isfile(self.__path):
-            try:
-                f = open(self.__path, 'r')
-                data = json.loads(f.read())
-                f.close()
-
-                self.__user = data['user']
-                self.__port = data['port']
-            # If there is a problem while trying to load the settings, we will inform the user and just
-            # use the default settings.
-            except json.JSONDecodeError:
-                tkinter.messagebox.showerror('JSON Error', 'Unable to decode the JSON settings file.')
-            except (OSError, IOError):
-                tkinter.messagebox.showerror('File Error', 'Unable to open the settings file for reading.')
-        # There is no settings file yet so we will create one with the default settings.
-        else:
-            self.save_settings(self.__user, self.__port)
+        self.__chat = None
 
         # Create the menu bar that goes across the top.
         menu = tkinter.Menu(self)
@@ -226,7 +147,7 @@ class ChatTk(tkinter.Tk):
         file = tkinter.Menu(menu, tearoff=False)
         file.add_command(label='Connect', command=self.__connect)
         file.add_command(label='Disconnect', command=self.__disconnect)
-        file.add_command(label='Host', command=self.__settings)
+        file.add_command(label='Host', command=self.__host)
         file.add_separator()
         file.add_command(label='Exit', command=self.quit)
         menu.add_cascade(label='File', menu=file)
@@ -236,27 +157,27 @@ class ChatTk(tkinter.Tk):
     def __connect(self):
         dialog = ConnectDialog(self)
         if not dialog.was_canceled():
-            pass
+            try:
+                if self.__chat is not None and not self.__chat.is_done():
+                    raise RuntimeError('Chat is already running.')
+                self.__chat = chat.Chat(dialog.get_username())
+                self.__chat.connect(dialog.get_address(), dialog.get_port())
+            except RuntimeError as e:
+                tkinter.messagebox.showerror('Error!', str(e))
 
     def __disconnect(self):
         pass
 
-    def __settings(self):
-        SettingsDialog(self)
-
-    def save_settings(self, user, port):
-        self.__user = user
-        self.__port = port
-        try:
-            f = open(self.__path, 'w')
-            data = {
-                'user': self.__user,
-                'port': self.__port
-            }
-            f.write(json.dumps(data))
-            f.close()
-        except (OSError, IOError):
-            tkinter.messagebox.showerror('File Error', 'Unable to write the requested settings.')
+    def __host(self):
+        dialog = ConnectDialog(self)
+        if not dialog.was_canceled():
+            try:
+                if self.__chat is not None and not self.__chat.is_done():
+                    raise RuntimeError('Chat is already running.')
+                self.__chat = chat.Chat(dialog.get_username())
+                self.__chat.host(dialog.get_address(), dialog.get_port())
+            except RuntimeError as e:
+                tkinter.messagebox.showerror('Error!', str(e))
 
     def get_username(self):
         return self.__user
